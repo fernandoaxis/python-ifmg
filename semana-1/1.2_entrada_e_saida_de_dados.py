c6794e2a28a8cefee69e398b77aa487aca4a0b4b
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-04-11 15:59:46 -0300

    Create 1.2_entrada_e_saida_de_dados.py

---
+++ semana-1/1.2_entrada_e_saida_de_dados.py
@@ -0,0 +1,11 @@
+print('Bem vindo')
+
+# Nome de usuário
+nome = input('Infome seu nome: ')
+
+# Idade convertida para inteiro
+idade = int(input('Infome sua idade'))
+
+# Escreve mensagem usando as variáveis
+print('Olá', nome, 'sua idade é ', idade)
+print('Até mais')
