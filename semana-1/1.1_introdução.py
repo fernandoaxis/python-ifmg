a89bd633e0017f0c2aae07fee74a3cfae558cf02
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-04-11 15:12:52 -0300

    Update 1.1_introdução.py

--- semana-1/1.1_introdução.py
+++ semana-1/1.1_introdução.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 	
-n1 = 10				#Número inteiro
-n2 = 2.5			#Numero fracionário
+n1 = 10			#Número inteiro
+n2 = 2.5		#Numero fracionário
 nome = 'Fernando'	#Textual
 teste = True		#Variável lógica
