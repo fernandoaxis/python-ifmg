f673af608e1ecf76109d9de4aa3210370e2f4b3d
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-04-11 16:49:21 -0300

    Add files via upload

---
+++ semana-1/1.3.1_operador+.py
@@ -0,0 +1,8 @@
+print('Informe os dados')
+
+nome = input('Nome: ')
+sobrenome = input('Sobrenome: ')
+idade = int(input('Idade: '))
+
+mensagem = nome + ' ' + sobrenome + ', ' + str(idade)
+print(mensagem)
